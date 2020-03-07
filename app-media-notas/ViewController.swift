10155401e407ca28ea36de7dd841b43e5ee0340c
Ismael Fernandes Pires <ismaa_gyn@hotmail.com>
2020-03-07 16:17:01 -0300

    Adicionando tela de splah e componentes

---
+++ app-media-notas/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  app-media-notas
+//
+//  Created by Professor SENAI on 3/7/20.
+//  Copyright © 2020 Faculdade Senai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        weak var lblTitulo: UILabel!
+        weak var actionCalcular: UIButton!
+        weak var lblResultado: UILabel!
+        weak var lblNota04: UITextField!
+        weak var lblNota03: UITextField!
+        weak var lblNota02: UITextField!
+        weak var lblNota01: UITextField!
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
